894ff60df46245ea7532e264e7ab10c1db9d78d9
jetstream1128 <bogdanalexeev2007@live.com>
2023-10-28 17:15:12 -0600

    v1.1

--- src/Start.js
+++ src/Start.js
@@ -2,7 +2,12 @@ function Start({ dispatch }) {
 	return (
 		<div className="start">
 			<h3>Hi there!</h3>
-			<p>Seem that it's your first time here. You need to open a new account</p>
+			<p>
+				It seems like you're new to this platform. In order to get started,
+				you'll need to create a new account. Just below this message, you'll
+				find a button – go ahead and click on it to begin the account creation
+				process. We're here to help you get started on your journey!
+			</p>
 			<button onClick={() => dispatch({ type: "start" })}>Open account</button>
 		</div>
 	);
